dc7a4736fa18657e71d5a44cd9a587ef243b8fd8
Jcode <chema_dh@hotmail.com>
2022-05-24 10:50:55 +0200

    Create seleccion.php
    Borrador menú selección.

---
+++ seleccion.php
@@ -0,0 +1,52 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <!--Se utiliza la etiqueta meta, como me se ve en la linea anteior, cuando se quiere optimizar para dispositivos móviles-->
+        <title>ChessWorld</title><!--Título de la pestaña-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Iconos de Font Awesome -->
+        <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+        <link rel="stylesheet" href="css/styles.css"><!--Acceso a nuestro css-->
+        <style>
+        body {background-color: black;}
+        </style>
+    </head>
+
+    <body>
+        <!-- Barra de navegación -->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="index.php">Inicio</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="contacto.php">Contacto</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Contacta -->
+        <section class="signup-section" id="signup">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-md-10 col-lg-8 mx-auto text-center">
+                        <i class="fa fa-gamepad fa-4x mb-2 text-white"></i>
+                        <h2 class="text-white mb-5">Selecciona un modo de aprendizaje</h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Pie de página -->
+        <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy; Chess World 2022</div></footer>
+        <!-- Efectos JS Bootstrap -->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
+
