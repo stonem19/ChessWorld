d19004bef95e068456649da57a9a4d99337df18e
Jcode <chema_dh@hotmail.com>
2022-06-07 00:49:59 +0200

    Estructurar documento SQL
    Estructurar el código y corregir usuarios duplicados.

--- bbdd.sql
+++ bbdd.sql
@@ -1,3 +1,4 @@
+/* TABLAS A CREAR */
 CREATE TABLE usuarios (
 nombre VARCHAR (25),
 pass VARCHAR (6),
@@ -28,27 +29,25 @@ nombre VARCHAR (25),
 correo VARCHAR (50),
 mensaje TEXT (500));
 
-INSERT INTO usuarios (nombre, pass, dni, aciertos, fallos, permisos, id) values ("User","123456","14512684H","0","0","CCC", uuid());
-INSERT INTO usuarios (nombre, pass, dni, aciertos, fallos, permisos, id) values ("User","789101","54848741K","0","0", "AAA", uuid());
+/* INSERTS EN BASE DE DATOS PARA COMPROBAR FUNCINAMIENTO */
+INSERT INTO usuarios (nombre, pass, dni, aciertos, fallos, permisos, id) values ("Usuario","123456","14512684H","0","0","CCC", uuid());
+INSERT INTO usuarios (nombre, pass, dni, aciertos, fallos, permisos, id) values ("Admin","789101","54848741K","0","0", "AAA", uuid());
 
 INSERT INTO permisos (tipo, codigo) values ("Administrador", "AAA");
 INSERT INTO permisos (tipo, codigo) values ("Empleado", "BBB");
 INSERT INTO permisos (tipo, codigo) values ("Usuario", "CCC");
 
+Insert into contacto (nombre, correo, mensaje) VALUES ("Pedro", "pedrochess@aol.es", "Me gustaría ahcer una colaboración publicitaria con vosotros. Nuestra web ofrece productos enfocados a jugadores de ajerez. Un saludo");
+insert into incidencias (nombre, correo, mensaje) values ("Jose", "josealfil@hotmail.com", "El formulario 2 del test, no registra cuando fallo. No es grave, pero me gustaría que funcionara. Gracias");
+
+/* Pruebas de consultas realizadas [NO EJECUTAR] */
 SELECT tipo, codigo from permisos;
-
 select id from usuarios where dni = "14512684H";
-
-select now();
-
 INSERT INTO acountuser (registro) VALUES(NOW());
-
 SELECT LEFT(UUID(), 6);
-
 select uuid() as uid;
-
 INSERT INTO freeuser (registro) VALUES(NOW());
 
-Insert into contacto (nombre, correo, mensaje) VALUES ("Pedro", "pedrochess@aol.es", "Me gustaría ahcer una colaboración publicitaria con vosotros. Nuestra web ofrece productos enfocados a jugadores de ajerez. Un saludo");
 
-insert into incidencias (nombre, correo, mensaje) values ("Jose", "josealfil@hotmail.com", "El formulario 2 del test, no registra cuando fallo. No es grave, pero me gustaría que funcionara. Gracias");
+
+
