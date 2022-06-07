760ebde4c940b8929652d092133b763742ba9db4
Jcode <chema_dh@hotmail.com>
2022-06-07 12:34:28 +0200

    Create conexion.php
    Archivo de conexión a bbdd para realizar pruebas con sesiones.

---
+++ conexion.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$database = "hlc_practicaweb";
+$username = "root";
+$password = "";
+
+$_SESSION["conn"] = new mysqli($servername, $username, $password, $database);
+if ($_SESSION["conn"]->connect_error) {
+    die("Connection failed: " . $_SESSION["con"]->connect_error);
+}
+?>
