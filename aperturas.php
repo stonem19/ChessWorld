069371e419f11c688c52ffc7d37c0f4ffb4ff2b5
Jcode <chema_dh@hotmail.com>
2022-06-08 00:18:32 +0200

    Correcciones estructura información
    Se sigue maquetando como se muestra la información sobre las aperturas de ajedrez.

--- aperturas.php
+++ aperturas.php
@@ -27,6 +27,20 @@
             font-weight: 700;
             padding: 0.9rem 0;
         }
+
+        .aperturas {
+            max-width: 80%;
+            max-height: 80%;
+            display: block;
+            margin: auto;
+        }
+
+        .aperturas2 {
+            max-width: 60%;
+            max-height: 60%;
+            display: block;
+            margin: auto;
+        }
     </style>
 </head>
 
@@ -35,19 +49,6 @@
     error_reporting(0);
     // Retomamos la sesión e indicamos que muestre por pantalla los datos de la misma
     require_once 'conexion.php';
-
-    /* Realizar registro incidencias*/
-    if (isset($_POST["nombre"])) {
-
-        $sql = "INSERT INTO incidencias (nombre, correo, mensaje) VALUES('" . $nombre . "', '" . $correo . "', '" . $mensaje . "')";
-
-        if (mysqli_query($_SESSION["con"], $sql) === TRUE) {
-            echo '<script type="text/javascript">', 'Swal.fire("Mensaje enviado");', '</script>';
-        } else {
-            echo "Error: " . $sql . "<br>" . $_SESSION["con"]->error;
-        }
-        mysqli_close($conn);
-    }
     ?>
     <!-- Barra de navegación -->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
@@ -56,34 +57,11 @@
             <a class="navbar-brand" href="seleccionNoUser.php">Selección</a>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link open-modal" data-open="modal">Reporta un problema</a></li>
                     <li class="nav-item"><a class="nav-link" href="contacto.php">Contacto</a></li>
                 </ul>
             </div>
         </div>
     </nav>
-    <!-- ACCIÓN DEL MODAL -->
-    <div class="modal" id="modal">
-        <div class="modal-dialog">
-            <header class="modal-header">
-                &nbsp;
-                <div data-close></div>
-            </header>
-            <section class="modal-content">
-                <p><strong>Reporta un problema</strong></p>
-                <form method="post" class="form-signup" name="incidencia" id="incidencia">
-                    <p>Introduce tu nombre</p>
-                    <input type="text" id="nombre" name="nombre" required />
-                    <p>Introduce correo de contacto</p>
-                    <input type="email" id="correo" name="correo" required />
-                    <p>Mensaje</p>
-                    <input type="text" id="mensaje" name="mensaje" required />
-                    <p>&nbsp;</p>
-                    <input type="submit" value="Enviar">
-                </form>
-            </section>
-        </div>
-    </div>
     <!-- Modales con aperturas -->
     <section class="projects-section bg-light" id="projects">
 
@@ -93,14 +71,14 @@
                 <h2 class="text-blackF mb-5">Aperturas de Ajedrez</h2>
             </div>
 
-            <!-- Modales con aperturas -->
+            <!-- Aperturas -->
             <div class="container py-4">
                 <div class="row align-items-md-stretch">
                     <div class="col-md-6">
                         <div class="h-100 p-5 text-white bg-dark rounded-3">
                             <h2>Defensa Nimzo-india</h2>
                             <p> Existe una gran cantidad de variantes, pero las ideas estratégicas son simples. Las negras tratarán de lanzar un ataque en el centro y en el ala de dama, mientras que las blancas lo harán en el ala de rey.</p>
-                            <button class="btn btn-outline-light" type="button" onclick="window.location='movimientos.php'">¡Quiero aprender!</button>
+                            <img src="assets\img\movimientos.png" class="aperturas">
                         </div>
                     </div>
                     <div class="col-md-6">
@@ -108,7 +86,7 @@
                             <!-- bg-light -->
                             <h2>Defensa Francesa</h2>
                             <p>La defensa francesa está encuadrada dentro de las aperturas semiabiertas. En conjunto, la defensa francesa es una muy buena apertura que ha sido utilizada por numerosos grandes jugadores a lo largo de la historia.</p>
-                            <button class="btn btn-outline-light" type="button" onclick="window.location=''">¡Enseñame!</button>
+                            <img src="assets\img\movimientos.png" class="aperturas">
                         </div>
                     </div>
                     <p>&nbsp;</p>
@@ -117,12 +95,13 @@
                             <!-- bg-light -->
                             <h2>Defensa Siciliana</h2>
                             <p>Goza de un gran prestigio entre los jugadores de cualquier nivel debido a su carácter agresivo, a la flexibilidad de las posiciones que otorga y, de manera significativa, al hecho de haber sido adoptada por varios campeones mundiales.</p>
-                            <button class="btn btn-outline-light" type="button" onclick="window.location=''">¡A la Siciliana!</button>
+                            <img src="assets\img\movimientos.png" class="aperturas2">
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
     </section>
-    </div>
     <!-- Pie de página -->
     <footer class="footer bg-black small text-center text-white-50">
         <div class="container px-4 px-lg-5">Copyright &copy; Chess World 2021</div>
