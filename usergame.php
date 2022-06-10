522c59e54b375ffe049f5662b45ea97722ad593e
Jcode <chema_dh@hotmail.com>
2022-06-11 01:01:04 +0200

    Introducción nueva tabla BBDD
    Tras introducir el nuevo filtro de datos, se crea una única tabla que contiene el contenido de las anteriores tablas de contacto e incidencias.

--- usergame.php
+++ usergame.php
@@ -57,7 +57,7 @@
     /* Realizar registro incidencias*/
     if (isset($_POST["nombre"])) {
 
-        $sql1 = "INSERT INTO incidencias (nombre, correo, mensaje) VALUES('" . $nombre . "', '" . $correo . "', '" . $mensaje . "')";
+        $sql1 = "INSERT INTO tramites (nombre, correo, tipo, mensaje) VALUES('" . $nombre . "', '" . $correo . "', 'Incidencia', '" . $mensaje . "')";
        
         if (mysqli_query($_SESSION["con"], $sql1) === TRUE) {
             echo '<script type="text/javascript">', 'Swal.fire("Mensaje enviado");', '</script>';
