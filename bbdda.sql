bf2183100518e40723eb8ed8bad7a359df8c36fe
Jcode <chema_dh@hotmail.com>
2022-06-11 00:49:48 +0200

    Corrección tablas bbdd
    Se corrigen las tablas de la bbdd, para optimizar la estructura. También se tienen que generar de nuevo una bbdd ya que la anterior da fallos de acceso, errores al ejecutar XAMPP y MySQL.

--- bbdda.sql
+++ bbdda.sql
@@ -1,15 +1,20 @@
 /* TABLAS A CREAR */
 CREATE TABLE usuarios (
-nombre VARCHAR (25),
+nombre VARCHAR (25) NOT NULL PRIMARY KEY,
 pass VARCHAR (6),
-dni VARCHAR (9),
+dni VARCHAR (9) UNIQUE,
 puntos BIGINT (5),
 permisos VARCHAR (3),
 id VARCHAR (36));
 
-CREATE TABLE contacto (
+CREATE TABLE acountuser (
+registro DATETIME (6),
+usuario VARCHAR (6));
+
+CREATE TABLE tramites (
 nombre VARCHAR (25),
 correo VARCHAR (50),
+tipo VARCHAR (10),
 mensaje TEXT (500));
 
 CREATE TABLE permisos (
@@ -17,18 +22,9 @@ tipo VARCHAR (25),
 codigo VARCHAR (3));
 
 CREATE TABLE freeuser (
-registro DATETIME (6));
-
-CREATE TABLE acountuser (
 registro DATETIME (6),
-usuario VARCHAR (6));
+hostinfo VARCHAR (10));
 
-CREATE TABLE incidencias (
-nombre VARCHAR (25),
-correo VARCHAR (50),
-mensaje TEXT (500));
-
-/* INSERTS EN BASE DE DATOS PARA COMPROBAR FUNCINAMIENTO */
 INSERT INTO usuarios (nombre, pass, dni, puntos, permisos, id) values ("Usuario","123456","14512684H","0","CCC", uuid());
 INSERT INTO usuarios (nombre, pass, dni, puntos, permisos, id) values ("Admin","789101","54848741K","0", "AAA", uuid());
 
@@ -39,6 +35,9 @@ INSERT INTO permisos (tipo, codigo) values ("Usuario", "CCC");
 Insert into contacto (nombre, correo, mensaje) VALUES ("Pedro", "pedrochess@aol.es", "Me gustaría ahcer una colaboración publicitaria con vosotros. Nuestra web ofrece productos enfocados a jugadores de ajerez. Un saludo");
 insert into incidencias (nombre, correo, mensaje) values ("Jose", "josealfil@hotmail.com", "El formulario 2 del test, no registra cuando fallo. No es grave, pero me gustaría que funcionara. Gracias");
 
+insert into tramites (nombre, correo, tipo, mensaje) values ("Jose", "josealfil@hotmail.com", "Incidencia", "El formulario 2 del test, no registra cuando fallo. No es grave, pero me gustaría que funcionara. Gracias");
+Insert into tramites (nombre, correo, tipo, mensaje) VALUES ("Pedro", "pedrochess@aol.es", "Contacto", "Me gustaría ahcer una colaboración publicitaria con vosotros. Nuestra web ofrece productos enfocados a jugadores de ajerez. Un saludo");
+
 /* Pruebas de consultas realizadas [NO EJECUTAR] */
 SELECT tipo, codigo from permisos;
 select id from usuarios where dni = "14512684H";
@@ -47,4 +46,9 @@ SELECT LEFT(UUID(), 6);
 select uuid() as uid;
 INSERT INTO freeuser (registro) VALUES(NOW());
 UPDATE usuarios set aciertos=aciertos+1 where nombre= 'Jose';
+
+insert into freeuser (registro, hostinfo) values(now(), connection_id());
+select host from information_schema.processlist WHERE ID=connection_id();
+INSERT INTO freeuser (registro, hostinfo) VALUES(NOW(), connection_id());
+select * from freeuser;
 
